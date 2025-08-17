4e9c7b62821c9414d71e5f36b821f90e64da7d18
Akshay <akshaykapsse@gmail.com>
2025-08-17 15:38:09 +0530

    For Vercel

--- app/layout.js
+++ app/layout.js
@@ -60,6 +60,7 @@ export default function RootLayout({ children }) {
           {children}
         </main>
         
+        
         {/* Footer */}
         <footer className="bg-gray-900 text-white py-12">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
